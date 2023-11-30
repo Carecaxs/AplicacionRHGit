ae84076c0edb303c06b8a50a19844fa6c1911675
Oscar <xscar1476@gmail.com>
2023-11-30 15:32:06 -0600

    cambiosss

---
+++ Models/Expedientes/DETALLE_EXPERIENCIA.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AplicacionRHGit.Models.Expedientes
+{
+
+    [Table("DETALLE_EXPERIENCIA")]
+    public class DETALLE_EXPERIENCIA
+    {
+        [Key]
+        public int ID_DETALLE_EXPERIENCIA { get; set; }
+        public int ID_EXPERIENCIA { get; set; }
+        public string NOMBRE_EMPRESA { get; set; }
+        public string DESCRIPCION_LABORES { get; set; }
+        public int inicio { get; set; }
+        public int fin { get; set; }
+        public string TELEFONO { get; set; }
+
+
+    }
+}
